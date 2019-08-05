95056feacec66d10e85a6bfa6cc6eb7d3cb772a1
MicahJank <MicahJank@users.noreply.github.com>
2019-08-04 20:34:52 -0500

    got the characters and the corresponding information to show up on the screen

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CharElement = styled.div`
-    width: 200px;
+    width: 400px;
     background-color: white;
     border-radius: 10px;
     padding: 20px;
@@ -14,7 +14,10 @@ const Character = ({ name, eyeColor, hairColor, gender }) => {
 
     return (
         <CharElement>
-
+            <h3>Name: {name}</h3>
+            <h3>Eye Color: {eyeColor}</h3>
+            <h3>Hair Color: {hairColor}</h3>
+            <h3>Gender: {gender}</h3>
         </CharElement>
     );
 };
