4fa65a326246496794cb82d9d8b3925f0fa2301f
MicahJank <MicahJank@users.noreply.github.com>
2019-08-04 20:51:26 -0500

    spaced out the information inside of the character elements better

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,22 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CharElement = styled.div`
-    width: 400px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 500px;
     background-color: white;
     border-radius: 10px;
     padding: 20px;
     margin: 20px auto;
+    text-align: left;
+`;
 
+const CharH3 = styled.h3`
+   
 `;
 
 const Character = ({ name, eyeColor, hairColor, gender }) => {
 
     return (
         <CharElement>
-            <h3>Name: {name}</h3>
-            <h3>Eye Color: {eyeColor}</h3>
-            <h3>Hair Color: {hairColor}</h3>
-            <h3>Gender: {gender}</h3>
+            <div>
+                <CharH3>Name: {name}</CharH3>
+                <CharH3>Eye Color: {eyeColor}</CharH3>
+            </div>
+            <div>
+                <CharH3>Hair Color: {hairColor}</CharH3>
+                <CharH3>Gender: {gender}</CharH3>
+            </div>
+            
         </CharElement>
     );
 };
