c3766526607b3603baed3e094ce12033a9f6fa57
MicahJank <MicahJank@users.noreply.github.com>
2019-08-04 22:53:56 -0500

    completed stretch - adding a pagination system

--- starwars/src/components/NextPageButton.js
+++ starwars/src/components/NextPageButton.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const NextPage = styled.button`
+    padding: 5px 20px;
+    margin: 30px;
+    border: none;
+    background-color: #DED2C2;
+    border-radius: 10px;
+    box-shadow: 0px 0px 18px 0px rgba(0,0,0,0.75);
+    cursor: pointer;
+`;
 
 const NextPageButton = ( { clickFunction }) => {
 
 
     return (
 
-        <button onClick={clickFunction}>Next Page</button>
+        <NextPage onClick={clickFunction}>Next Page</NextPage>
     );
 };
 
