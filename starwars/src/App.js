4ad9225e774471da7262e37041d0339cf00a9436
MicahJank <MicahJank@users.noreply.github.com>
2019-08-04 20:19:19 -0500

    set up my state and created a useEffect hook where i can set the state to the object in the api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import axios from 'axios';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [characters, setCharacters] = useState([]);
+
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+
+    axios.get('https://swapi.co/api/people')
+      .then(dataObj => {
+        console.log('dataObj', dataObj);
+
+        setCharacters(dataObj.data.results); // characters is now an array of people
+      })
+
+      .catch(err => {
+        alert(err);
+      });
+
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <div>
+        
+      </div>
     </div>
   );
 }
